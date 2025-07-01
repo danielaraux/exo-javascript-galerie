4e9e76ed17ad38bb88e3d466991d61fa585f90ca
daniel <danielaraux@hotmail.com>
2025-07-01 11:07:22 +0200

    squelette

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,31 @@ let nbPhotos = 16
 
 
 function generateRandomImages() {
-    for (i = 0; i < nbPhotos; i++) {
-        img = document.createElement(img)
+
+    for (index = 1; index < nbPhotos; index++) {
+        let randomIndex = Math.floor(Math.random() * 100)
+
+        document.getElementById("img-div").innerHTML += `<a class="images" href="https://picsum.photos/id/${randomIndex}/200/300.webp" data-lightbox="example-set"
+                data-title="Click the right half of the image to move forward."><img class="example-image"
+                    src="https://picsum.photos/id/${randomIndex}/200/300.webp" alt="" /></a>`
     }
+
+
+    // for (index = 0; index < nbPhotos; index++) {
+    //     document.getElementById("img-div").innerHTML += `<a class="images" href="https://picsum.photos/500/700?random=${index}" data-lightbox="example-set"
+    //             data-title="Click the right half of the image to move forward."><img class="example-image"
+    //                 src="https://picsum.photos/500/700?random=${index}" alt="" /></a>`
+    // }
 }
+
+
+
+
+
+
+generateRandomImages()
+
+
+// // for (let index = 0; index < word.length; index++) {
+// document.getElementById("guessWord").innerHTML += `
+//     <div id="letter-${index}" class="letter"></div>
